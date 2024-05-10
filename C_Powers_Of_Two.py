74f5e3d6ae0dc221689890e7b2a550e60354271e
yakobsolo <yakisolo2009@gmail.com>
2024-05-10 23:49:37 +0300

    div 4

---
+++ C_Powers_Of_Two.py
@@ -0,0 +1,97 @@
+
+import re
+import functools
+import random
+import sys
+import os
+import math
+from collections import Counter, defaultdict, deque
+from functools import lru_cache, reduce, cmp_to_key
+from itertools import accumulate, combinations, permutations
+from heapq import nsmallest, nlargest, heappushpop, heapify, heappop, heappush
+from io import BytesIO, IOBase
+from copy import deepcopy
+import threading
+from typing import *
+from operator import add, xor, mul, ior, iand, itemgetter
+import bisect
+from string import ascii_lowercase, ascii_uppercase
+BUFSIZE = 4096
+inf = float('inf')
+
+def I():
+    return input()
+ 
+def II():
+    return int(input())
+ 
+def MII():
+    return map(int, input().split())
+ 
+def LI():
+    return list(input().split())
+ 
+def LMII():
+    return list(map(int,  input().split()))
+
+ 
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+ 
+def LGMI():
+    return list(map(lambda x: int(x) - 1, input().split()))
+ 
+
+
+# check if a number is prime
+def isPrime(x: int) -> bool:
+   d = 2
+   while d * d <= x:
+       if x % d == 0:
+           return False
+       d += 1
+   return True
+
+
+#  returns prime factorization of a number
+
+
+def Primefactorization(n: int) -> list[int]:
+    factorization = []
+       
+    d = 2
+
+    while d * d <= n:
+        while n % d == 0:
+            factorization.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+    	  factorization.append(n)
+  
+    return factorization
+ 
+
+
+
+N, K = MII()
+S=bin(N)[2:]
+bitcount = S.count("1")
+
+L = list(map(int, reversed(S)))
+if bitcount>K or K>N:
+    print("NO")
+else:
+    print("YES")
+    while(bitcount<K):
+        if L[-1]==0:
+            L.pop()
+        L[-1]-=1
+        L[-2]+=2
+        bitcount+=1
+    for i in range(len(L)):
+        print((str(2**i)+" ")*L[i],end="" )
+
+
+
+
