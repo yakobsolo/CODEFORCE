def6ec1444e536b2504e513b7ead55a4b2163e4a
yakobsolo <yakisolo2009@gmail.com>
2024-05-13 22:44:12 +0300

    b and is'ts non zero

---
+++ B_And_It_s_Non_Zero.py
@@ -0,0 +1,94 @@
+
+import re
+import functools
+import random
+import sys
+import os
+import math
+from collections import Counter, defaultdict, deque
+from functools import lru_cache, reduce, cmp_to_key
+from itertools import accumulate, combinations, permutations
+from heapq import nsmallest, nlargest, heappushpop, heapify, heappop, heappush
+from io import BytesIO, IOBase
+from copy import deepcopy
+import threading
+from typing import *
+from operator import add, xor, mul, ior, iand, itemgetter
+import bisect
+from string import ascii_lowercase, ascii_uppercase
+BUFSIZE = 4096
+inf = float('inf')
+
+def I():
+    return input()
+ 
+def II():
+    return int(input())
+ 
+def MII():
+    return map(int, input().split())
+ 
+def LI():
+    return list(input().split())
+ 
+def LMII():
+    return list(map(int,  input().split()))
+
+ 
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+ 
+def LGMI():
+    return list(map(lambda x: int(x) - 1, input().split()))
+ 
+
+
+# check if a number is prime
+def isPrime(x: int) -> bool:
+   d = 2
+   while d * d <= x:
+       if x % d == 0:
+           return False
+       d += 1
+   return True
+
+
+
+
+
+#  returns prime factorization of a number
+
+
+def Primefactorization(n: int) -> list[int]:
+    factorization = []
+       
+    d = 2
+
+    while d * d <= n:
+        while n % d == 0:
+            factorization.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+    	  factorization.append(n)
+  
+    return factorization
+ 
+
+
+M = 2 * 10**5
+cnt = [[0]*19 for i in range(M+1)]
+for i in range(1,M+1):
+    for j in range(19):
+        cnt[i][j] = cnt[i-1][j] + (i>>j & 1)
+
+
+
+for _ in range(int(input())):
+    l,r = MII()
+ 
+    res = r-l+1
+    for j in range(19):
+        tmp = (r-l+1) - (cnt[r][j]-cnt[l-1][j])
+        res = min(res,tmp)
+    print(res)
