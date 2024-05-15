23835f15d9d394c2b921f23ca3d3d8551ef30db6
yakobsolo <yakisolo2009@gmail.com>
2024-05-15 22:32:35 +0300

    NIT

---
+++ A_We_Need_the_Zero.py
@@ -0,0 +1,105 @@
+
+import re
+import functools
+import random
+import sys
+import os
+import math
+from collections import Counter, defaultdict, deque
+from functools import lru_cache, reduce, cmp_to_key
+from itertools import accumulate, combinations, permutations
+from heapq import nsmallest, nlargest, heappushpop, heapify, heappop, heappush
+from io import BytesIO, IOBase
+from copy import deepcopy
+import threading
+from typing import *
+from operator import add, xor, mul, ior, iand, itemgetter
+import bisect
+from string import ascii_lowercase, ascii_uppercase
+BUFSIZE = 4096
+inf = float('inf')
+
+def I():
+    return input()
+ 
+def II():
+    return int(input())
+ 
+def MII():
+    return map(int, input().split())
+ 
+def LI():
+    return list(input().split())
+ 
+def LMII():
+    return list(map(int,  input().split()))
+
+ 
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+ 
+def LGMI():
+    return list(map(lambda x: int(x) - 1, input().split()))
+ 
+
+
+# check if a number is prime
+def isPrime(x: int) -> bool:
+   d = 2
+   while d * d <= x:
+       if x % d == 0:
+           return False
+       d += 1
+   return True
+
+
+
+
+
+#  returns prime factorization of a number
+
+
+def Primefactorization(n: int) -> list[int]:
+    factorization = []
+       
+    d = 2
+
+    while d * d <= n:
+        while n % d == 0:
+            factorization.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+    	  factorization.append(n)
+  
+    return factorization
+ 
+
+
+for _ in range(II()):
+    N = II()
+    arr = LMII()
+    mx = max(arr)
+    res = [[0, 0] for i in range(mx.bit_length())]
+    ans = 0
+    flg = True
+    # print(len(res), "len")
+    for i in range(len(res)):
+        # print(res[i], "be")
+        for val in arr:
+            if 1<<i & val:
+                res[i][0] +=1
+            else:
+                res[i][1] +=1
+        # print(res[i],i)
+        if res[i][0]%2 != 0:
+            
+            if res[i][1] %2 == 0:
+                ans +=1<<i
+            else:
+                flg = False
+                break
+    if flg: print(ans)
+    else:
+        print(-1)
+
