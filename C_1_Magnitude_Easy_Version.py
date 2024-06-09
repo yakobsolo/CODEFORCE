0bdf4a618fb98ff4b6240b8c1a44b20e53b802d2
yakobsolo <yakisolo2009@gmail.com>
2024-06-09 20:40:44 +0300

    global round

---
+++ C_1_Magnitude_Easy_Version.py
@@ -0,0 +1,112 @@
+
+import re
+import functools
+import random
+import sys
+import os
+import math
+from collections import Counter, defaultdict, deque
+from functools import lru_cache, reduce, cmp_to_key
+from itertools import accumulate, combinations, permutations
+from heapq import nsmallest, nlargest, heappushpop, heapify, heappop, heappush
+from io import BytesIO, IOBase
+from copy import deepcopy
+import threading
+from typing import *
+from operator import add, xor, mul, ior, iand, itemgetter
+import bisect
+from string import ascii_lowercase, ascii_uppercase
+BUFSIZE = 4096
+inf = float('inf')
+
+def I():
+    return input()
+ 
+def II():
+    return int(input())
+ 
+def MII():
+    return map(int, input().split())
+ 
+def LI():
+    return list(input().split())
+ 
+def LMII():
+    return list(map(int,  input().split()))
+
+ 
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+ 
+def LGMI():
+    return list(map(lambda x: int(x) - 1, input().split()))
+ 
+
+
+# check if a number is prime
+def isPrime(x: int) -> bool:
+   d = 2
+   while d * d <= x:
+       if x % d == 0:
+           return False
+       d += 1
+   return True
+
+
+
+
+
+#  returns prime factorization of a number
+
+
+def Primefactorization(n: int) -> list[int]:
+    factorization = []
+       
+    d = 2
+
+    while d * d <= n:
+        while n % d == 0:
+            factorization.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+    	  factorization.append(n)
+  
+    return factorization
+ 
+
+for _ in range(II()):
+    n = II()
+    arr = LMII()
+    ans = [0]
+    stack = [(0, 0)]
+    
+
+
+    visted = set()
+    dp = [[0, 0] for i in range(n)]
+    while stack:
+        ind, path = stack.pop()
+
+        if ind +1 == n:
+            ans[0] = max(ans[0], abs(path + arr[ind]))
+            continue
+        # visted.add(path+arr[ind])
+        stack.append((ind + 1, path + arr[ind]))
+        # if path + arr[ind] > dp[ind][1]:
+            
+        #     dp[ind][1] = path + arr[ind]
+        if path + arr[ind] < 0 :
+            if  path + arr[ind] < dp[ind][0]: 
+        
+                stack.append((ind + 1, abs(path + arr[ind])))
+                dp[ind][0] = path  + arr[ind]
+        
+        
+        
+
+        
+            
+
+   
+    print(ans[0])
